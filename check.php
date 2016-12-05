fb840024c4a0c7c060e69eb521783983b12c4f95
iShinta <ho_minh_tri@yahoo.fr>
2016-12-05 00:40:07 -0600

    Reponse 16

--- check.php
+++ check.php
@@ -1,23 +1,16 @@
 <?php
-  echo "SNSD";
   $username = $_GET["username"];
-  echo "Yoona";
   $file = fopen("passwd", "r");
-  echo "Taeyeon";
   $userlist = Array();
   while(!feof($file)){
-    echo "SNSD while ";
     $line = fgets($file);
     $line_pieces = explode(":", $line);
     $userlist[$line_pieces[0]] = $line_pieces[1];
   }
   fclose($file);
-  echo "SNSD2";
-  if(!array_key_exists($username, $userlist)){
+  if(array_key_exists($username, $userlist)){
     $response = "true";
-    echo "fjlsdkf";
   }else{
     $response = "false";
-    echo "lsjdflkdsjl";
   }
 ?>
