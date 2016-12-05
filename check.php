adc8bf850aea613aa7df9e9cd22cdf11db0ff3c3
iShinta <ho_minh_tri@yahoo.fr>
2016-12-05 00:30:32 -0600

    Reponse 9

--- check.php
+++ check.php
@@ -5,16 +5,18 @@
   $file = fopen("passwd", "r");
   $userlist = Array();
   while(!feof($file)){
+    echo "SNSD while ";
     $line = fgets($file);
     $line_pieces = explode(":", $line);
     $userlist[$line_pieces[0]] = $line_pieces[1];
   }
   fclose($file);
+  echo "SNSD2";
   if(!array_key_exists($username, $userlist)){
     $response = "true";
-    echo $response;
+    echo "fjlsdkf";
   }else{
     $response = "false";
-    echo $response;
+    echo "lsjdflkdsjl";
   }
 ?>
