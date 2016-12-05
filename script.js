3398a3ddba9ade5534d0830ea36ec636d5078fe3
iShinta <ho_minh_tri@yahoo.fr>
2016-12-04 23:34:41 -0600

    Full debug

--- script.js
+++ script.js
@@ -5,23 +5,28 @@ var xhr;
     else if (window.XMLHttpRequest) {
         xhr = new XMLHttpRequest ();
     }
-} 
+    alert("XHR created");
+}
 
 function checkName(){
   var username = document.getElementById("username").value;
+  alert(username);
   if(username == null || username == "") return;
-
+  alert("Passed empty return");
   var url = "check.php?username="+escape(username);
-
+  alert(url);
   xhr.open("GET", url, true);
   xhr.onreadystatechange = updatePage;
   xhr.send(null);
 }
 
 function updatePage() {
+  alert("updatePage");
     if (xhr.readyState == 4) {
         if (xhr.status == 200) {
+          alert("xhr passed");
             var response = xhr.responseText;
+            alert(response);
             if(response == "true") window.alert("Username already taken.");
         }
     }
