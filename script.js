eab1b501c7d8b0e595ac80e19cb948a8fa2c545b
iShinta <ho_minh_tri@yahoo.fr>
2016-12-04 23:52:23 -0600

    no more comment

--- script.js
+++ script.js
@@ -6,25 +6,18 @@ if (window.ActiveXObject) {
 }
 
 function checkName(){
-  window.alert("checkName");
   var username = document.getElementById("username").value;
-  window.alert(username);
   if(username == null || username == "") return;
-  window.alert("Passed empty return");
   var url = "check.php?username="+escape(username);
-  window.alert(url);
   xhr.open("GET", url, true);
   xhr.onreadystatechange = updatePage;
   xhr.send(null);
 }
 
 function updatePage() {
-  window.alert("updatePage");
     if (xhr.readyState == 4) {
         if (xhr.status == 200) {
-          window.alert("xhr passed");
             var response = xhr.responseText;
-            window.alert(response);
             if(response == "true") window.alert("Username already taken.");
         }
     }
