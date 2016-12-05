ac8678dd5194a24f127f8ca9280a1989abb58e94
iShinta <ho_minh_tri@yahoo.fr>
2016-12-05 00:02:46 -0600

    Moins de comms

--- script.js
+++ script.js
@@ -6,16 +6,11 @@ if (window.ActiveXObject) {
 }
 
 function checkName(username){
-  window.alert(username);
   if(username == null || username == "") return;
   var url = "check.php?username="+escape(username);
-  window.alert(url);
   xhr.open("GET", url, true);
-  window.alert("open");
   xhr.onreadystatechange = updatePage();
-  window.alert("update");
   xhr.send(null);
-  window.alert("send");
 }
 
 function updatePage() {
