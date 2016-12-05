35ab9475f19e8477cb0197132e2ebf64bf53d772
iShinta <ho_minh_tri@yahoo.fr>
2016-12-05 00:09:38 -0600

    Plus de update

--- script.js
+++ script.js
@@ -9,18 +9,14 @@ function checkName(username){
   if(username == null || username == "") return;
   var url = "check.php?username="+escape(username);
   xhr.open("GET", url, true);
+  xhr.onreadystatechange = function() {
+    if ((xhr.readyState == 4) && (xhr.status == 200)) {
+      window.alert("xhr passed");
+      var response = xhr.responseText;
+      window.alert(response);
+      if(response == "true") window.alert("Username already taken.");
+    }
+	};
   xhr.onreadystatechange = updatePage();
   xhr.send(null);
-}
-
-function updatePage() {
-  window.alert("updatePage");
-    if (xhr.readyState == 4) {
-        if (xhr.status == 200) {
-          window.alert("xhr passed");
-            var response = xhr.responseText;
-            window.alert(response);
-            if(response == "true") window.alert("Username already taken.");
-        }
-    }
 }
