59ff9af0eff2ec37759a2f2a9fce83f507e36cf2
iShinta <ho_minh_tri@yahoo.fr>
2016-12-05 00:22:42 -0600

    Reponse 3

--- script.js
+++ script.js
@@ -9,10 +9,12 @@ function checkName(username){
   if(username == null || username == "") return;
   var url = "check.php?username="+escape(username);
   xhr.open("GET", url, true);
+  window.alert("open");
   xhr.onreadystatechange = function() {
+    window.alert("readyState");
     if ((xhr.readyState == 4) && (xhr.status == 200)) {
       window.alert("xhr passed");
-      var response = xhr.response;
+      var response = xhr.responseText;
       window.alert(response);
       if(response == "true") window.alert("Username already taken.");
     }
